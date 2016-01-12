b330eff8131d6bfdda28f7ec34a3642ac6dd53cd
Sadjad Fouladi <sfouladi@gmail.com>
2016-01-11 17:33:52 -0800

    Use libx264 to compute SSIM values.

--- src/Bitmap.cpp
+++ src/Bitmap.cpp
@@ -3,6 +3,7 @@
 #include <zlib.h>
 #include <iostream>
 #include <fstream>
+#include <x264.h>
 
 Bitmap::Bitmap(AVFrame *frame) {
 	pix_fmt = static_cast<PixelFormat>(frame->format);
@@ -138,19 +139,41 @@ void Bitmap::dumpPGM(const char *filename) {
 	file.close();
 }
 
-extern "C" {
-	typedef uint8_t pixel;
-	float x264_pixel_ssim_wxh( pixel *pix1, intptr_t stride1,
-							pixel *pix2, intptr_t stride2,
-							int width, int height, void *buf, int *cnt );
+struct x264_pixel_function_t
+{
+  // 158 is the number of pointers in this internal x264 struct
+  // this is obviously a bit fragile since x264 could add more
+  void * ptrs[ 158 ];
+};
+
+// These are internal libx264 instructions so they aren't included in x264.h
+extern "C"
+{
+  float x264_pixel_ssim_wxh( const x264_pixel_function_t * func, const uint8_t *pix1,
+                             uintptr_t stride1, const uint8_t *pix2, uintptr_t stride2,
+                             int width, int height, void *buf, int *cnt );
+
+  void x264_pixel_init( int cpu, x264_pixel_function_t *pixf );
+
+  uint32_t x264_cpu_detect( void );
 }
+
+x264_pixel_function_t init_pixel_function( void )
+{
+  x264_pixel_function_t pix_func;
+  x264_pixel_init( x264_cpu_detect(), &pix_func );
+
+  return pix_func;
+}
+
+x264_pixel_function_t x264_funcs = init_pixel_function();
 
 double Bitmap::SSIM(Bitmap &other) {
 	uint8_t *tmpbuf = new uint8_t[8192*1024];
 	int count;
 	double ssim;
 
-	ssim = x264_pixel_ssim_wxh(picture.data[0], picture.linesize[0],
+	ssim = x264_pixel_ssim_wxh(&x264_funcs, picture.data[0], picture.linesize[0],
 	                           other.picture.data[0], other.picture.linesize[0],
 	                           width, height, tmpbuf+16, &count);
 
